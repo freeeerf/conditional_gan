67b9fd4d826d485b0b6eb8487221d00893bc33db
larry <liuchangyu1111@gmail.com>
2024-12-11 14:44:00 +0800

    Update conv_net.py

--- conditional_gan/models/conv_net.py
+++ conditional_gan/models/conv_net.py
@@ -35,7 +35,7 @@ class ConvNet(nn.Module):
         )
 
         self.backbone = nn.Sequential(
-            nn.ConvTranspose2d(self.latent_dim, 512, (6, 6), stride=(1, 1), padding=(0, 0), bias=False),
+            nn.ConvTranspose2d(self.latent_dim, 512, (4, 4), stride=(1, 1), padding=(0, 0), bias=False),
             nn.BatchNorm2d(512),
             nn.ReLU(True),
 
@@ -47,11 +47,11 @@ class ConvNet(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(128, 64, (3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+            nn.ConvTranspose2d(128, 64, (4, 4), stride=(1, 1), padding=(0, 0), bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(64, self.channels, (4, 4), stride=(2, 2), padding=(1, 1)),
+            nn.ConvTranspose2d(64, self.channels, (4, 4), stride=(2, 2), padding=(0, 0)),
 
             nn.Tanh()
         )
